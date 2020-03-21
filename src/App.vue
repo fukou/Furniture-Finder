381c694ffd06e58af9291619d7c86ac60d2d6b75
Faiz Ichsan Jaya <faluvtha@gmail.com>
2020-03-21 14:04:46 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app class="wrapper">
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  color: #111;
+}
+
+body {
+  background-color: #eee;
+}
+
+.wrapper {
+  margin: 5rem auto;
+  max-width: 60rem;
+  background: transparent !important;
+}
+
+@media (max-width: 60rem) {
+  .wrapper {
+    margin: 1rem auto;
+  }
+}
+
+.chips > * + * {
+  margin-left: 0.65rem;
+}
+</style>
